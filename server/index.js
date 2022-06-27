704e27a1362aadea8495fe8dafa82d126cb89d97
Idan Lev <idan.93lev@gmail.com>
2022-06-27 17:34:48 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const http = require("http");
 const path = require("path");
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/routes.js');
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/routes.js");
 
-const publicPath = path.join(__dirname, "../client/build");
+// const publicPath = path.join(__dirname, "../client/build");
 
 const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3001;
@@ -14,16 +14,19 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended:true }))
-app.use(express.static(publicPath));
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(publicPath));
 app.use(userRoutes);
 
 mongoose.connect(uri, () => console.log("connected to mongoDB"));
 
-app.use("*", (req, res) => {
-  res.sendFile(path.resolve(`${publicPath}/index.html`));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`server runs on port ${PORT}`);
-})
+});
