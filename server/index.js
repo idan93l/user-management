0062db602c4932f41d35e8040646bff7663fbf87
Idan Lev <idan.93lev@gmail.com>
2022-06-27 15:58:43 +0300

    build installed in client

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const http = require("http");
+const path = require("path");
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/routes.js');
+
+const publicPath = path.join(__dirname, "../client/build");
 
 const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3001;
@@ -11,6 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }))
+app.use(express.static(publicPath));
 app.use(userRoutes);
 
 mongoose.connect(uri, () => console.log("connected to mongoDB"));
